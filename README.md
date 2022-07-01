b77e4838852618bcd9cc3f429910749549cae069
danzilla0x <8739617+danzilla0x@users.noreply.github.com>
2022-07-01 23:08:42 +0300

    Add script to rename grafana hostname

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ```
 curl -s 'https://raw.githubusercontent.com/danzilla0x/tools/master/.profile' > ~/.profile
 ```
+
+2. Runn script to rename Grafana hostname:
+```
+curl -Os https://raw.githubusercontent.com/danzilla0x/tools/master/rename_grafana_hostname.sh && bash rename_grafana_hostname.sh
+```
---
+++ rename_grafana_hostname.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# HOSTNAME=$1
+# if [ -z "$1" ]; then
+#     echo "No new hostname provided."
+#     exit
+# fi
+read -p "Type new hostname: " HOSTNAME
+
+YML_PATH="/etc/prometheus/prometheus.yml"
+if [ ! -z "$1" ]; then
+    YML_PATH="$1"
+fi
+
+if [ -f $YML_PATH ]; then
+    sudo sed -i "s/hostname:.*/hostname: '$HOSTNAME'/g" $YML_PATH
+    sudo sed -i "s/replacement:.*/replacement: '$HOSTNAME'/g" $YML_PATH
+else
+    echo "No prometeus.yml file to update."
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl enable vmagent && sudo systemctl restart vmagent
+sudo systemctl enable node_exporter && sudo systemctl restart node_exporter
